c9f3c8c074202f0b2f24e5aa61dba33ab01eb866
sdhave <sdhave@usiu.ac.ke>
2022-11-20 18:26:51 +0200

    Frontend v2 to be created

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a77691923143d4852a778d74b28de85c-1007259095.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
